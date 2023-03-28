ca12282eb824cc450ed78cf32af97957dd5314dc
ANDRES ALEXIS MALFAVAUN <2203024384@alumnos.cua.uam.mx>
2023-03-28 16:20:39 -0600

    ultimas modificaciones me quede en el minuto 1:42:02

--- secciones/usuarios/crear.php
+++ secciones/usuarios/crear.php
@@ -1,3 +1,41 @@
 <?php include("../../templates/header.php");?>
 <h1>crear usuarios</h1>
+
+<br>
+    <div class="card">
+        <div class="card-header">
+            Datos del Usuarios
+        </div>
+        <div class="card-body">
+
+            <form action="" method="post" enctype="multipart/form-data">
+
+                <div class="mb-3">
+                  <label for="usuario" class="form-label">Nombre del Usuario</label>
+                  <input type="text"
+                    class="form-control" name="usuario" id="usuario" aria-describedby="helpId" placeholder="Nombre del Usuario" Required>
+                </div>
+
+                <div class="mb-3">
+                  <label for="correo" class="form-label">Correo</label>
+                  <input type="email"
+                    class="form-control" name="correo" id="correo" aria-describedby="helpId" placeholder="escribe tu correo" Required>
+                  <small id="helpId" class="form-text text-muted">Si ta estas registrado inicia sesion <a href="#">aqui</a></small>
+                </div>
+
+                <div class="mb-3">
+                  <label for="password" class="form-label">password:</label>
+                  <input type="password"
+                    class="form-control" name="password" id="password" aria-describedby="helpId" placeholder="Escribe tu contraseña" Required>
+                </div>
+
+                <button type="submit" class="btn btn-success">Agregar</button>
+                <a name="" id="" class="btn btn-primary" href="index.php" role="button">Cancelar</a>
+            </form>
+
+        </div>
+        <div class="card-footer text-muted">
+        </div>
+    </div>
+
 <?php include("../../templates/footer.php");?>
--- secciones/usuarios/index.php
+++ secciones/usuarios/index.php
@@ -1,3 +1,37 @@
 <?php include("../../templates/header.php");?>
-<h1>lista usuarios</h1>
+
+<br>
+<div class="card">
+    <div class="card-header ">
+        <a name="" id="" class="btn btn-primary" href="crear.php" role="button">Agregar Usuarios</a>
+    </div>
+    <div class="card-body">
+    <div class="table-responsive">
+    <table class="table ">
+        <thead>
+            <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Nombre del Usuario</th>
+                <th scope="col">Contraseña</th>
+                <th scope="col">Correo</th>
+                <th scope="col">Acciones</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr class="">
+                <td scope="row">1</td>
+                <td>Andres</td>
+                <td>********</td>
+                <td>alex@gmail.com</td>
+                <td>
+                    <input name="btneditar" id="btneditar" class="btn btn-warning text-white" type="button" value="Editar">
+                    <input name="btnborrar" id="btnborrar" class="btn btn-danger text-white" type="button" value="Eliminar">
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+
+    </div>
+</div>
 <?php include("../../templates/footer.php");?>
